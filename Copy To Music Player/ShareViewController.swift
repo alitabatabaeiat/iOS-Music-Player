232822bf5820c41744518e110fdafd2b672aa78c
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-12-13 14:20:43 +0330

    refactoring completion handler of loadItem

--- Copy To Music Player/ShareViewController.swift
+++ Copy To Music Player/ShareViewController.swift
@@ -37,39 +37,40 @@ class ShareViewController: UIViewController {
         
         for (_, attachment) in attachments.enumerated() {
             if attachment.hasItemConformingToTypeIdentifier(contentType) {
-                attachment.loadItem(forTypeIdentifier: contentType, options: nil) { [weak self] data, error in
-                    if error == nil, let url = data as? URL {
-                        do {
-                            let rawData = try Data(contentsOf: url)
-                            let sharedURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.ir.alitabatabaei.Music-Player")
-                            let dir = sharedURL?.appendingPathComponent("sharedSongs")
-                            let songURL = dir?.appendingPathComponent(url.lastPathComponent)
-                            guard let path = songURL?.path else { return }
-                            FileManager.default.createFile(atPath: path, contents: rawData, attributes: nil)
-                        }
-                        catch let exp {
-                            print("GETTING EXCEPTION \(exp.localizedDescription)")
-                        }
-                        
-                    } else {
-                        print("GETTING ERROR")
-                        let alert = UIAlertController(title: "Error", message: "Error loading song", preferredStyle: .alert)
-                        
-                        let action = UIAlertAction(title: "Error", style: .cancel) { _ in
-                            self?.dismiss(animated: true, completion: nil)
-                        }
-                        
-                        alert.addAction(action)
-                        self?.present(alert, animated: true, completion: nil)
-                    }
-                    self?.redirectToHostApp()
-                }
+                attachment.loadItem(forTypeIdentifier: contentType, options: nil, completionHandler: self.loadItemCallback(data:error:))
             }
         }
     }
     
+    func loadItemCallback(data: NSSecureCoding?, error: Error?) {
+        if error == nil, let url = data as? URL {
+            do {
+                let rawData = try Data(contentsOf: url)
+                let sharedURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.ir.alitabatabaei.Music-Player")
+                let dir = sharedURL?.appendingPathComponent("sharedSongs")
+                let songURL = dir?.appendingPathComponent(url.lastPathComponent)
+                guard let path = songURL?.path else { return }
+                FileManager.default.createFile(atPath: path, contents: rawData, attributes: nil)
+            }
+            catch let exp {
+                print("GETTING EXCEPTION \(exp.localizedDescription)")
+            }
+            
+        } else {
+            print("GETTING ERROR")
+            let alert = UIAlertController(title: "Error", message: "Error loading song", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Error", style: .cancel) { _ in
+                self.dismiss(animated: true, completion: nil)
+            }
+            
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        self.redirectToHostApp()
+    }
+    
     @objc func redirectToHostApp() {
-        print("here")
         let url = URL(string: "MusicPlayer://")
         var responder = self as UIResponder?
         let selectorOpenURL = sel_registerName("openURL:")
